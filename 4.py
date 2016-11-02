90ab11fe6ae80a7222ffc8605b42fbf3027c944e
Denis <denisoleshenko@gmail.com>
2016-11-02 19:53:47 +0200

    Create 4.py

---
+++ 4.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python3
+
+for i in range(0,30000):
+	if(i%11 == 0 and i % 2 == 1 and i % 3 == 1 and i % 4 == 1 and i % 5 == 1 and i % 6 == 1
+				 and i % 7 == 1 and i % 8 == 1 and i % 9 == 1 and i % 10 == 1):
+		print(i)
